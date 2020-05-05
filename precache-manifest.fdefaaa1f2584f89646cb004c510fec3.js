6a464cfada052c2af4f6392be90a9ad69dfd84c0
Jacek <trojanowski.jacek@gmail.com>
2020-05-05 17:44:22 +0200

    Updates

---
+++ precache-manifest.fdefaaa1f2584f89646cb004c510fec3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f744476ee5a9a51f61ef9460f2bf3eb",
+    "url": "/react-router-app/index.html"
+  },
+  {
+    "revision": "8968bcf9c53aa0809706",
+    "url": "/react-router-app/static/css/main.f6480caa.chunk.css"
+  },
+  {
+    "revision": "db72ddc70f6743025cd9",
+    "url": "/react-router-app/static/js/2.6a9ba453.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-router-app/static/js/2.6a9ba453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8968bcf9c53aa0809706",
+    "url": "/react-router-app/static/js/main.e53cc46b.chunk.js"
+  },
+  {
+    "revision": "3333c725fc4664d7ef64",
+    "url": "/react-router-app/static/js/runtime-main.5af8bed5.js"
+  },
+  {
+    "revision": "010f8a3253c1a56507125456406d4c45",
+    "url": "/react-router-app/static/media/poznan1.010f8a32.jpg"
+  },
+  {
+    "revision": "ad6e495bec1a748e5f5d4b6f746bbdfa",
+    "url": "/react-router-app/static/media/poznan2.ad6e495b.jpg"
+  },
+  {
+    "revision": "69d9f9ce714f66cb5fb8569be44d347e",
+    "url": "/react-router-app/static/media/poznanCathedral.69d9f9ce.jpg"
+  },
+  {
+    "revision": "9884956f681167b3868c7afb66b58deb",
+    "url": "/react-router-app/static/media/poznanTrainStation.9884956f.jpg"
+  }
+]);
